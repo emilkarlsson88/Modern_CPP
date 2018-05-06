4e0bba3a51c0f013da138280e29542fca353ac69
EmilKarlsson <emilkarlsson88@gmail.com>
2018-05-06 16:30:54 +0200

    modified the orderloaded ex with something not so obvius

--- modern_cpp_ex/compiler_warnings/ex/Woverloaded-virtual.cpp
+++ modern_cpp_ex/compiler_warnings/ex/Woverloaded-virtual.cpp
@@ -3,27 +3,25 @@
 
 class Base {
 public:
- explicit Base(std::string t_status) : m_status(t_status) {
-    std::cout << "ctor Base" << std::endl;
-  }
-  ~Base() { std::cout << "~Base" << std::endl; }
-  virtual void print() { std::cout << "From Base: " << m_status << std::endl; }
+ explicit Base(const std::string& t_status) : m_status{t_status} {}
+  virtual ~Base() {}
+  virtual void print() const { std::cout << "From Base: " << m_status << std::endl; }
 
-private:
+protected:
   std::string m_status;
 };
 
-class DerivedTwo : public Base {
+class Derived : public Base {
 public:
- explicit DerivedTwo(std::string t_status) : Base(t_status) {
-    std::cout << "ctor DerivedTwo" << std::endl;
-  }
-  ~DerivedTwo() { std::cout << "~DerivedTwo" << std::endl; }
-  void print(std::string& status) { std::cout << "From DerivedTwo: " << status << std::endl; }
+ explicit Derived(const std::string& t_status) : Base{t_status} {}
+  void print() { std::cout << "From Derived: " << m_status << std::endl; }
 };
 
 int main() {
-    std::unique_ptr<Base> baseinterface =
-        std::make_unique<DerivedTwo>("Opss! I'm not DerivedTwo!");
-    baseinterface->print();
+    std::unique_ptr<Base> base_base = std::make_unique<Base>("I'm Base!");
+    std::unique_ptr<Derived> derived_derived = std::make_unique<Derived>("I'm Derived!");
+    std::unique_ptr<Base> base_derived = std::make_unique<Derived>("I'm Derived!");
+    base_base->print();
+    derived_derived->print();
+    base_derived->print();
 }

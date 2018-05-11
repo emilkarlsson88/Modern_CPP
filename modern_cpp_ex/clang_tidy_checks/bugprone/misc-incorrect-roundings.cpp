78a1fd034cfad758aa50ca22b965843ea144277e
EmilKarlsson <emilkarlsson88@gmail.com>
2018-05-11 22:04:02 +0200

    added rounding bug

---
+++ modern_cpp_ex/clang_tidy_checks/bugprone/misc-incorrect-roundings.cpp
@@ -0,0 +1,17 @@
+#include<iomanip>
+#include<iostream>
+
+int my_round(float value){
+	return (int)(value + 0.5f);
+}
+
+void print(float value){
+	std::cout << "my_round(" << std::setprecision(10) << value << ") = " << my_round(value) << '\n';
+}
+
+int main() {
+	float val_one{0.499999975f};
+	float val_two = -1.4f;
+	print(val_one);
+	print(val_two);
+}
